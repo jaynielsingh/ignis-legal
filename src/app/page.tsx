26732d7db114b91c91afed0616b80b0af6b03539
Shawn <jaynielsingh@yahoo.com>
2024-09-28 20:41:24 -0700

    add pricing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Feature from "./_components/features";
 import Footer from "./_components/footer";
 import CTA from "./_components/cta";
 import Contact from "./_components/contact";
+import Pricing from "./_components/pricing";
 // import Values from "./_components/values";
 
 export default function HomePage() {
@@ -12,8 +13,9 @@ export default function HomePage() {
       <Hero />
       <Feature />
       {/* <Values /> */}
-      <Contact/>
       <CTA />
+      <Pricing />
+      <Contact />
       <Footer />
     </main>
   );
